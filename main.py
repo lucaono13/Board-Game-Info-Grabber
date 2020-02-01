a9f3c08a0afd8d274b083be39090ab434f9988f8
lucaono13 <gionorato@gmail.com>
2020-02-01 14:36:11 -0800

    New Features (selection, addition, removal)

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ from api_functions import query
 import sys
 import urllib
 data = []
-#data.append(('Catan','1995','13','No'))
+
+
 class mywindow(QtWidgets.QMainWindow):
+    gamesToBeAdded = []
+    ids_toAdd = []
     def __init__(self):
         super(mywindow,self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.searchResults.setRowCount(1)
+        self.ui.searchResults.setRowCount(0)
         self.ui.searchResults.setColumnCount(4)
         self.ui.searchResults.setHorizontalHeaderLabels(('Name','Year','UID','Type'))
         header = self.ui.searchResults.horizontalHeader()
@@ -22,6 +25,8 @@ class mywindow(QtWidgets.QMainWindow):
 
         self.ui.searchButton.clicked.connect(self.api_search)
         self.ui.searchLine.returnPressed.connect(self.api_search)
+        self.ui.addButton.clicked.connect(self.addIDs)
+        self.ui.removeButton.clicked.connect(self.removeIDs)
 
     def api_search(self):
         answers = query(self.ui.searchLine.text(), self.ui.gameCheck.isChecked(), self.ui.expanCheck.isChecked())
@@ -43,13 +48,60 @@ class mywindow(QtWidgets.QMainWindow):
                 col += 1
             row += 1
         header = self.ui.searchResults.horizontalHeader()
-        header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        #header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
         header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeToContents)
         data.clear()
 
+    def addIDs(self):
+        addedGames = []
+        for idx in self.ui.searchResults.selectionModel().selectedRows():
+            if(self.ui.searchResults.item(idx.row(),2).text() not in self.ids_toAdd):
+                self.ids_toAdd.append(self.ui.searchResults.item(idx.row(),2).text())
+                name = self.ui.searchResults.item(idx.row(),0).text()
+                year = self.ui.searchResults.item(idx.row(),1).text()
+                self.gamesToBeAdded.append("%s (%s)" % (name, year))
+                addedGames.append("%s (%s)" % (name, year))
+            else:
+                print("%s already in list" % (self.ui.searchResults.item(idx.row(),0).text()))
 
+        #print("")
+        #print(self.ui.selectedGamees.count())
+        #self.ui.selectedGamees.setRowCount(len(gamesToBeAdded))
+        row = 0
+        for game in addedGames:
+            #info = QListWidgetItem(game)
+            self.ui.selectedGamees.insertItem(self.ui.selectedGamees.count(),game)
+            #row += 1
+        addedGames.clear()
+
+    def removeIDs(self):
+        #listedgames = gamesToBeAdded
+
+        for j in range(len(self.ids_toAdd)):
+            print(self.gamesToBeAdded[j], self.ids_toAdd[j])
+
+
+        r_idxs = []
+        for idx in self.ui.selectedGamees.selectionModel().selectedRows():
+            gamename = self.ui.selectedGamees.item(idx.row()).text()
+            r_idxs.append(self.gamesToBeAdded.index(gamename))
+            self.gamesToBeAdded = [x for x in self.gamesToBeAdded if x is not gamename]
+
+        for item in self.ui.selectedGamees.selectedItems():
+            self.ui.selectedGamees.takeItem(self.ui.selectedGamees.row(item))
+
+        for i in r_idxs:
+            del self.ids_toAdd[i]
+
+        print("")
+        for j in range(len(self.ids_toAdd)):
+            print(self.gamesToBeAdded[j], self.ids_toAdd[j])
+
+    def refreshList(self):
+        pass
 
 
 app = QtWidgets.QApplication([])
