c9d79499a70cbd18ffee269cb74ab575869a0fc4
lucaono13 <gionorato@gmail.com>
2020-02-08 11:26:38 -0800

    Minor Updates

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class infoWindow(QtWidgets.QMainWindow):
         info.clear()
         info.update([('id',self.ui.bggUID.isChecked()), ('age',self.ui.age.isChecked()), ('players',self.ui.nPlayers.isChecked()), ('name',self.ui.name.isChecked()), ('playtime',self.ui.playTime.isChecked()), ('year',self.ui.year.isChecked()), ('artists',self.ui.artists.isChecked()), ('categories',self.ui.categories.isChecked()), ('designers',self.ui.designers.isChecked()), ('expans',self.ui.expans.isChecked()), ('mechanisms',self.ui.mechanisms.isChecked()), ('expans_c',self.ui.nExpan.isChecked()), ('publisher',self.ui.publisher.isChecked()), ('bbg_rank',self.ui.bgg_rank.isChecked()), ('bestPlayers',self.ui.bnPlayers.isChecked()), ('complexity',self.ui.complexity.isChecked()), ('ratings_c',self.ui.nRatings.isChecked()), ('rating',self.ui.rating.isChecked()), ('image',self.ui.image.isChecked()), ('description',self.ui.description.isChecked()), ('link',self.ui.bgg_link.isChecked())])
         #print(info)
-        self.csv_preview = pd.DataFrame()
+        #self.csv_preview = pd.DataFrame()
 
         self.csv_preview = self.csv_preview.append(grabInfo(info, Window.ids_toAdd))
         #print(self.csv_preview)
